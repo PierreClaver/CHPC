0291c388983f0bbd550f08aafc3136b6c842615b
PierreClaver <30508276+PierreClaver@users.noreply.github.com>
2017-12-03 11:23:58 +0200

    Delete iptables_migration.sh

--- IPtables/iptables_migration.sh
+++
@@ -1,9 +0,0 @@
-read -p "Press ENTER to transfer from firewalld to iptables"
-
-systemctl stop firewalld
-systemctl mask firewalld
-yum -y install iptables-services
-systemctl enable iptables
-
-systemctl start iptables
-
