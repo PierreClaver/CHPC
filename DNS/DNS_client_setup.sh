148236d1037351b0d970d5644b3ae13a0b566170
PierreClaver <30508276+PierreClaver@users.noreply.github.com>
2017-12-03 11:21:57 +0200

    Delete DNS_client_setup.sh

--- DNS/DNS_client_setup.sh
+++
@@ -1,9 +0,0 @@
-sudo chattr -i /etc/resolv.conf
-sudo echo "search gekko.cluster.local" > /etc/resolv.conf
-sudo echo "nameserver 10.0.0.1" >> /etc/resolv.conf
-sudo chattr +i /etc/resolv.conf
-
-echo "node1 or node2"
-read -p "Give node name > " NODE_NAME
-DOMAIN_NAME=".gekko.cluster.local"
-sudo echo $NODE_NAME$DOMAIN_NAME > /etc/hostname
